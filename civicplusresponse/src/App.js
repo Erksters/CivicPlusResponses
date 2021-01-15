ffd1997c4bea82141aef0d2629d0106104172ed3
Erick PC <erksters@gmail.com>
2021-01-14 19:17:54 -0600

    Added font size slider

--- civicplusresponse/src/App.js
+++ civicplusresponse/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Response1 from './components/Response1/Response1';
+import Slider from 'react-input-slider';
+import { useState } from 'react';
+import {Row, Col} from 'reactstrap'
 
 function App() {
+  const [sliderValue, setSliderValue] = useState(20)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+    <Row>
+      <Col></Col>
+      <Col xs={8}>
+        <Row>
+          <Col style={{textAlign:'right'}}>
+             <label className="label-fontsize">Font Size &nbsp;</label>
+          </Col>
+          <Col>
+            <Slider 
+              className="Slider"
+              axis="x"
+              x={sliderValue}
+              xstep={1}
+              xmin={16}
+              xmax={32}
+              onChange={({x}) =>{
+                setSliderValue(x)
+                }}
+            />
+          </Col>  
+        </Row>
+        
+        {/* <header className="header">Hello</header> */}
+        <Response1 sliderFontSize={`${sliderValue.toString()}px`} />
+    
+      </Col>
+      <Col></Col>
+    </Row>      
+      
     </div>
   );
 }
