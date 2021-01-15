770840d7377563fe3abb0e5f9989689b4baaab92
Erick PC <erksters@gmail.com>
2021-01-14 20:09:22 -0600

    Added fade in animation from right

--- civicplusresponse/src/App.js
+++ civicplusresponse/src/App.js
@@ -3,15 +3,16 @@ import Response1 from './components/Response1/Response1';
 import Slider from 'react-input-slider';
 import { useState } from 'react';
 import {Row, Col} from 'reactstrap'
+import FadeInSection from './components/FadeInObject/FadeInObject';
 
 function App() {
-  const [sliderValue, setSliderValue] = useState(20)
+  const [sliderValue, setSliderValue] = useState(26)
 
   return (
     <div className="homepage">
     <Row>
       <Col></Col>
-      <Col xs={8}>
+      <Col xs={7}>
         <Row>
           <Col style={{textAlign:'right'}}>
              <label className="label-fontsize">Font Size &nbsp;</label>
@@ -30,10 +31,11 @@ function App() {
             />
           </Col>  
         </Row>
+        <FadeInSection ><Response1 sliderFontSize={`${sliderValue.toString()}px`} /></FadeInSection>
+        <FadeInSection ><Response1 sliderFontSize={`${sliderValue.toString()}px`} /></FadeInSection>
+        <FadeInSection ><Response1 sliderFontSize={`${sliderValue.toString()}px`} /></FadeInSection>
+        <FadeInSection ><Response1 sliderFontSize={`${sliderValue.toString()}px`} /></FadeInSection>
         
-        {/* <header className="header">Hello</header> */}
-        <Response1 sliderFontSize={`${sliderValue.toString()}px`} />
-    
       </Col>
       <Col></Col>
     </Row>      
