38f4820aeb3de716895fc64c091dee604c397e88
Erick PC <erksters@gmail.com>
2021-01-17 15:51:45 -0600

    Measuring the screen width just in case

--- civicplusresponse/src/components/MobileWarning/MobileWarningAlert.js
+++ civicplusresponse/src/components/MobileWarning/MobileWarningAlert.js
@@ -6,8 +6,8 @@ import swal from 'sweetalert';
 const MobileWarningAlert = () => {
     const warningMsg = `This page was designed for desktops! It may not render correctly on your screen due to it's size.`
     const [goAway, setGoAway] = useState(true);
-    
-    if(goAway){
+    console.log(window.innerWidth)
+    if(window.innerWidth < 500 && goAway){
         swal("Screen too small?", warningMsg, "info");
         setGoAway(false)
     }
